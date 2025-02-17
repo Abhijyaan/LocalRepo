f31fa18f296a1c59d0d39c9d11d37ffb91d83f37
Abhijyaan <abhijyaandabral707@gmail.com>
2025-02-17 21:44:12 +0530

    ...

---
+++ firsthtml/LabWork/prac8.js
@@ -0,0 +1,6 @@
+console.log("This is a console log message.");
+document.write("<p>This is written using document.write().</p>");
+
+window.alert("This is an alert message!");
+
+document.getElementById("output").innerHTML = "This is written using innerHTML.";
